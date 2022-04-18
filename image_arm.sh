4db0b44bbdabe15f09a9c406cbd69615807ea6c6
Robert Diers <robertdiers@gmail.com>
2022-04-18 14:16:27 +0200

    docker for armhf

--- image_arm.sh
+++ image_arm.sh
@@ -1,2 +1,2 @@
-podman build --tag docker.io/robertdiers/solarmanager:arm .
-podman push docker.io/robertdiers/solarmanager:arm
+docker build --tag docker.io/robertdiers/solarmanager:arm .
+docker push docker.io/robertdiers/solarmanager:arm
