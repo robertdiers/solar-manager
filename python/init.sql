fba37896a2913c2ba2a2272449df7d020c8715f4
Robert Diers <robertdiers@gmail.com>
2022-08-26 15:52:47 +0200

    Update init.sql

--- python/init.sql
+++ python/init.sql
@@ -8,4 +8,5 @@ SELECT add_retention_policy('metrics', INTERVAL '7 days');
 CREATE TABLE voltages ( "time" timestamptz NOT NULL, "key" varchar NOT NULL, "value" double precision );
 SELECT create_hypertable('voltages', 'time');
 ALTER TABLE voltages SET ( timescaledb.compress, timescaledb.compress_segmentby = 'time' );
-SELECT add_retention_policy('voltages', INTERVAL '1 days');
+SELECT add_compression_policy('voltages', INTERVAL '1 days');
+SELECT add_retention_policy('voltages', INTERVAL '7 days');
